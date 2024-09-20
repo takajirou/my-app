203631f05992a9b8c606382dbbcb8142fa9950d8
高木 湊二郎 <>
2024-09-20 14:54:09 +0900

    todo 終了

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -43,6 +43,30 @@ function TodoList() {
             </ul>
         </div>
     );
+
+    //mapの場合
+    //コードが一致しますか？という疑問系
+    // const deleteTodo = (indexToDelete: number) => {
+    //     setTodos(todos.map((todo, index) => (index === indexToDelete ? "" : todo)));
+    // };
+
+    // return (
+    //     <div>
+    //         {/* テキスト入力フィールド、newTodoの値が変わるたびに状態を更新 */}
+    //         <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+    //         {/* ボタンを押すと新しいTodoが追加される */}
+    //         <button onClick={addTodo}>Add Todo</button>
+    //         <ul>
+    //             {/* todosの配列の各要素をmapで処理して、TodoItemコンポーネントを生成している */}
+    //             {todos.map((todo, index) =>
+    //                 todo ? (
+    //                     //TodoItemにtodoを渡して表示、keyには配列のインデックスを使用
+    //                     <TodoItem key={index} todo={todo} onDelete={() => deleteTodo(index)} />
+    //                 ) : null
+    //             )}
+    //         </ul>
+    //     </div>
+    // );
 }
 
 export default TodoList;
