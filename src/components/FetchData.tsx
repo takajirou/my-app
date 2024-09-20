825d297a3c8f61f019a4e739f965b3fe397f0630
高木 湊二郎 <>
2024-09-20 15:53:27 +0900

    APIend

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 
 import { fetchPosts } from "./apiClient";
 
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
+
 function FetchData() {
     //状態変数dataを定義。初期値は空。
     //APIから取得したデータをこの変数に保存する。
-    const [data, setData] = useState<any[]>([]);
+    const [data, setData] = useState<Post[]>([]);
     //useEffectフックを使って、コンポーネントがマウントされたときにfetchPosts関数を呼び出す
     //空の依存配列[]を渡しているので、この処理はコンポーネントの最初の描画時に一度だけ実行される
     useEffect(() => {
